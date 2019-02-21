8d55edf03d4a4c686df603a4850d97ef009e5c21
Shima <shima.jin@icloud.com>
2019-02-21 15:01:43 +0900

    prepare

---
+++ Feuille/Feuille.h
@@ -0,0 +1,19 @@
+//
+//  Feuille.h
+//  Feuille
+//
+//  Created by jinsei_shima on 2019/02/21.
+//  Copyright © 2019 Jinsei Shima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Feuille.
+FOUNDATION_EXPORT double FeuilleVersionNumber;
+
+//! Project version string for Feuille.
+FOUNDATION_EXPORT const unsigned char FeuilleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feuille/PublicHeader.h>
+
+
